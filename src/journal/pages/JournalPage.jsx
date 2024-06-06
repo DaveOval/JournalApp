2978257328b6662affff89ee0a0ac2b26d743633
David Vazquez <dave_u@outlook.com>
2024-06-05 23:17:39 -0600

    feat: Add dispatch for new note creation in JournalPage
    This commit adds the dispatch function to create a new note in the JournalPage. When the user clicks the "New Note" button, the startNewNote action is dispatched, triggering the creation of a new note in the journal.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,14 +2,24 @@ import { AddOutlined, MailOutline } from "@mui/icons-material"
 import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../view/"
+import { useDispatch } from "react-redux"
+import { startNewNote } from "../../store/journal"
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() );
+  }
+
   return (
     <JournalLayout>
       {/* <NothingSelectedView /> */}
       {/* <NoteView /> */}
 
       <IconButton
+        onClick={onClickNewNote}
         size="large"
         sx={{
           color: "white",
